9377c9b308d144aeed3210f83557eb2770fdaea1
Mishelle Osorio <mishelleosorioh@gmail.com>
2026-08-20 22:27:21 -0500

    clase 2

--- src/main/java/pe/edu/pe/PharmaBackend/service/generic/CrudService.java
+++ src/main/java/pe/edu/pe/PharmaBackend/service/generic/CrudService.java
@@ -1,7 +1,5 @@
 package pe.edu.pe.PharmaBackend.service.generic;
 
-import java.util.Optional;
-
 public interface CrudService <REQ,RES,ID>{
     RES create(REQ t);
     RES update(ID id, REQ t);
--- src/main/java/pe/edu/pe/PharmaBackend/service/service/CategoriaService.java
+++ src/main/java/pe/edu/pe/PharmaBackend/service/service/CategoriaService.java
@@ -1,6 +1,5 @@
 package pe.edu.pe.PharmaBackend.service.service;
 
-import org.springframework.transaction.annotation.Transactional;
 import pe.edu.pe.PharmaBackend.dto.CategoriaRequestDTO;
 import pe.edu.pe.PharmaBackend.dto.CategoriaResponseDTO;
 import pe.edu.pe.PharmaBackend.service.generic.CrudService;
